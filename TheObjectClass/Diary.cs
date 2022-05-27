13b9bb44a8d20f7a2b1035db9906f173527af358
Tuğrul Sübekci <tugrulsubekci@gmail.com>
2022-05-27 19:23:25 +0300

    Add project files.

---
+++ TheObjectClass/Diary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectClass
+{
+    internal class Diary : Book, IFlippable
+    { 
+        public int CurrentPage
+        { get; set; }
+
+        public Diary(int page = 0) : base()
+        {
+            CurrentPage = page;
+        }
+
+        public Diary(int page, string author, string title) : base(author, title)
+        {
+            CurrentPage = page;
+        }
+
+        public void Flip()
+        {
+            CurrentPage++;
+        }
+
+        public string SpillSecret()
+        {
+            return "OMG kerry loves kris <3";
+        }
+
+        public override string Stringify()
+        {
+            return "This is a Diary object!";
+        }
+        //Overriding Object Members
+        public override string ToString()
+        {
+            return "Surprise!";
+        }
+    }
+}
---
+++ TheObjectClass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Object Type
+            // Every class is derived from Object.
+            Book bk = new Book();
+
+            Diary dy = new Diary(38);
+
+            int i = 9;
+
+            Object o1 = bk;
+            Object o2 = dy;
+            Object o3 = i;
+
+            // Object Members
+            Book b = new Book();
+            Diary d = new Diary(38);
+            Random r = new Random();
+
+            Object[] ArrayO = new Object[] { b, d, r, i };
+
+            foreach (Object o in ArrayO)
+            {
+                Console.WriteLine(o.GetType());
+            }
+
+            //Overriding Object Members
+            Book bkk = new Book("Ta-Nehisi Coates", "Between the World and Me");
+            Console.WriteLine(bk.ToString());
+
+            // Object in Plain Sight
+            Diary diary1 = new Diary();
+            Console.WriteLine(diary1);
+
+
+            //to keep console open
+            Console.ReadLine();
+
+        }
+    }
+}
